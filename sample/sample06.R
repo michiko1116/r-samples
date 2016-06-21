1aee2c7581ea41092e039c6185d088b417beae4f
michiko1116 <1116.s.michiko@gmail.com>
2016-06-21 22:48:01 +0900

    内容の編集

--- sample/sample06.R
+++ sample/sample06.R
@@ -1,11 +1,11 @@
 # 行列
 
-x <- matrix(1:6, nrow=3, ncol=2)
-x <- matrix(1:6, nrow=3, ncol=2, byrow=TRUE)
-x <- rbind(c(1,2), 3:4, 5:6)
-x <- cbind(c(1,2), 3:4, 5:6)
+x <- matrix(1:6, nrow=3, ncol=2) # ベクトルを行列に変換
+x <- matrix(1:6, nrow=3, ncol=2, byrow=TRUE) # 数字を横に並べる
+x <- rbind(c(1,2), 3:4, 5:6) # データを行でバインド
+x <- cbind(c(1,2), 3:4, 5:6) # データを列でバインド
 x + 1
 1 / x
-dim(x)
-nrow(x)
-ncol(x)
+dim(x) # 行と列の数を表示
+nrow(x) # 行の数を表示
+ncol(x) # 列の数を表示
